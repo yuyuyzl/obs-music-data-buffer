8dee098b9695192ea62fca741a50f2da25858c34
yuyuyzl <yuzhongliang221@gmail.com>
2022-04-22 02:59:15 +0800

    remove comments

--- src/App.js
+++ src/App.js
@@ -2,30 +2,8 @@ import './App.css';
 import {useEffect, useState} from "react";
 
 const host='http://localhost:8899';
-// const getRB=(key)=>fetch(host+'/get/'+key).then(res=>res.text());
-
 
 function App() {
-  // const [songName,setSongName]=useState();
-  // const [songBy,setSongBy]=useState();
-  // const [curTime,setCurTime]=useState();
-  // const [curTimePercent,setCurTimePercent]=useState();
-  // const [totTime,setTotTime]=useState();
-  // const [img,setImg]=useState();
-  // const [lyricOriginal,setLyricOriginal]=useState();
-  // const [lyricTrans,setLyricTrans]=useState();
-  // useEffect(()=>{
-  //   setInterval(()=>{
-  //     getRB('songname').then(setSongName);
-  //     getRB('songby').then(setSongBy);
-  //     getRB('currenttime').then(setCurTime);
-  //     getRB('currenttimepercent').then(setCurTimePercent);
-  //     getRB('tottime').then(setTotTime);
-  //     getRB('img').then(setImg);
-  //     getRB('lyricorig').then(setLyricOriginal);
-  //     getRB('lyrictrans').then(setLyricTrans);
-  //   },1000);
-  // },[]);
   const [data,setData]=useState();
   useEffect(()=>{
     const ii=setInterval(()=>fetch(host+'/get/json').then(res=>res.json()).then(setData),200);
